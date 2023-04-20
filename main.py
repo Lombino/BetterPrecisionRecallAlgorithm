db0671509b69c58cf9c178120cc427b473d82373
TommiLombardi <tommasolombardi94@gmail.com>
2023-04-20 17:11:42 +0200

    added class specific p r and f1

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from metric_utils import *
 
 
 if __name__ == '__main__':
-    output = np.random.randint(5, size=(15, 50))
-    gt = np.random.randint(5, size=(15, 50))
+    output = np.random.randint(2, size=(15, 50))
+    gt = np.random.randint(2, size=(15, 50))
 
     p, r, f1 = multiclass_multioutput_overall_precision_recall_f1score(output, gt)
+    print("OVERALL precision = {} recall = {} f1_score = {}\n".format(p, r, f1))
 
-    print("precision = {} \nrecall = {} \nf1_score = {}".format(p, r, f1))
+    cp, cr, cf1 = multiclass_multioutput_class_specific_p_r_f1(output, gt)
+
+    for i, (p, r, f1) in enumerate(zip(cp, cr, cf1)):
+        print("CLASS {}:   precision = {} recall = {} f1_score = {}\n".format(i, p, r, f1))
+
+
 
 
 
--- metric_utils.py
+++ metric_utils.py
@@ -35,3 +35,22 @@ def multiclass_multioutput_overall_precision_recall_f1score(y_true, y_pred, zero
         f1_score = zero_division
 
     return precision, recall, f1_score
+
+
+def multiclass_multioutput_class_specific_p_r_f1(y_true, y_pred):
+    """
+    :param y_true: Ground truth. Expecting a [n,m] shaped np.array
+    :param y_pred: Output predicted. Expecting a [n,m] shaped np.array
+    :return: 3 [n] shaped arrays containing the precision, recall and f1 score of each class
+    """
+    aus = np.logical_and(y_true, y_pred)
+    # aus is an [n,m] shaped tensor where
+    # the (i,j) element is 1 if the j'th output of the i'th input is correctly classified
+    class_specific_tp = np.sum(aus, axis=1)
+    tp_plus_fp = np.sum(y_pred, axis=1)
+    tp_plus_fn = np.sum(y_true, axis=1)
+    class_specific_p = np.round(class_specific_tp / tp_plus_fp, decimals=4)
+    class_specific_r = np.round(class_specific_tp / tp_plus_fn, decimals=4)
+    class_specific_f1 = 2 * (class_specific_p * class_specific_r)/(class_specific_p + class_specific_r)
+    class_specific_f1 = np.round(class_specific_f1, decimals=4)
+    return class_specific_p, class_specific_r, class_specific_f1
