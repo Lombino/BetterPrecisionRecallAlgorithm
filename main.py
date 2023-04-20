1c91a8ce84405ff344f2998ee95a0fb5bf24fd92
TommiLombardi <tommasolombardi94@gmail.com>
2023-04-20 12:13:53 +0200

    Multiclass Multioutput precision recall f1  function implemented

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import sklearn.metrics
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def new_algorithm_precision_recall_f1score(y_true, y_pred):
+    aus = np.logical_and(y_true, y_pred)
+    p_intersec_target_ = np.sum(aus)  # cardinality
+    p_ = np.sum(y_pred)  # cardinality
+    t_ = np.sum(y_true)  # cardinality
+
+    try:
+        precision = round(p_intersec_target_ / p_, 4)
+    except ZeroDivisionError:
+        precision = 0
+    try:
+        recall = round(p_intersec_target_ / t_, 4)
+    except ZeroDivisionError:
+        recall = 0
+    if precision + recall > 0:
+        mul = precision * recall
+        sum = precision + recall
+        f1_score = round(2 * mul / sum, 4)
+    else:
+        f1_score = 0
+
+    return precision, recall, f1_score
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    output = np.random.randint(5, size=(15, 50))
+    gt = np.random.randint(5, size=(15, 50))
+
+    p, r, f1 = new_algorithm_precision_recall_f1score(output, gt)
+
+    print("precision = {} \nrecall = {} \nf1_score = {}".format(p, r, f1))
+
+    ps, rs, f1s, _ = sklearn.metrics.classification_report(y_true=gt, y_pred=output, zero_division=0)
+    print("precision = {} \nrecall = {} \nf1_score = {}".format(ps, rs, f1s))
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
